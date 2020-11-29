4cb9813289d7e75ca6ed5fc83619bcf591f4daaa
AlexTanS <tanaev@bk.ru>
2020-11-29 13:04:34 +0300

    Add Register user`s

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path, include
 
-from .views import blog, profile, BlogLoginView, BlogLogoutView
+from .views import blog, profile, BlogLoginView, BlogLogoutView, RegisterUserView, RegisterDoneView
 
 app_name = "blog"
 
 urlpatterns = [
+    path("accounts/register/done/", RegisterDoneView.as_view(), name="register_done"),
+    path("accounts/register/", RegisterUserView.as_view(), name="register"),
     path("accounts/logout/", BlogLogoutView.as_view(), name="logout"),  # log out of account
     path("accounts/profile/", profile, name="profile"),  # page 'my_story'
     path("accounts/login/", BlogLoginView.as_view(), name="login"),  # page 'login'
--- blog/views.py
+++ blog/views.py
@@ -2,21 +2,31 @@ from django.shortcuts import render
 from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.decorators import login_required
+from django.views.generic.edit import CreateView
+from django.views.generic.base import TemplateView
+from django.contrib.auth.models import User
+from django.urls import reverse_lazy
+
+from .forms import RegisterUserForm
+
 
 #  blog home page
 # [rus] главная страница блога
 def blog(request):
     return render(request, 'blog.html')
 
+
 # the page controller 'login'
 # [rus] контроллер страницы 'входа'
 class BlogLoginView(LoginView):
     template_name = "login.html"
 
+
 # the page controller 'exit'
 # [rus] контроллер страницы 'выхода'
 class BlogLogoutView(LoginRequiredMixin, LogoutView):
     template_name = "logout.html"
+
 
 # profile for the user who logged in: shows his stories
 # [rus] профиль для пользователя (выполневшего вход): показывает его рассказы
@@ -24,3 +34,17 @@ class BlogLogoutView(LoginRequiredMixin, LogoutView):
 def profile(request):
     return render(request, "profile.html")
 
+
+# controller-class that registers the user
+# [rus] контроллер-класс регистрирующий пользователя
+class RegisterUserView(CreateView):
+    model = User
+    template_name = "register_user.html"
+    form_class = RegisterUserForm
+    success_url = reverse_lazy("blog:register_done")
+
+
+# displays a message about successful registration
+# [rus] выводит сообщение об удачной регистрации
+class RegisterDoneView(TemplateView):
+    template_name = "register_done.html"
